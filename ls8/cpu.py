986d363d03e3e9eef1c8842c1ddc1bd60dc2ae8e
Mr. Disney <joshua.a.disney@gmail.com>
2019-10-17 13:47:19 -0700

    CALL and RET working.  MVP for the week

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -12,6 +12,7 @@ class CPU:
         self.ram = [0] * 256
         self.register = [0] * 8
         self.register[7] = 0xF4
+        self.SP = self.register[7]
         self.PC = 0
         self.FL = 0
 
@@ -103,15 +104,17 @@ class CPU:
     def run(self):
         """Run the CPU."""
         HLT  = 0b00000001
+        RET  = 0b00010001
         PUSH = 0b01000101
         POP  = 0b01000110
         PRN  = 0b01000111
+        CALL = 0b01010000
         LDI  = 0b10000010
         ADD  = 0b10100000
         SUB  = 0b10100001
         MUL  = 0b10100010
         DIV  = 0b10100011
-        MOD  = 0b10100100 #<-------- No clue what this does yet.  I'll have to find it in the spec.
+        MOD  = 0b10100100 #<-------- No clue how to do this yet.  I'll have to look it up.
 
 
         running = True
@@ -146,18 +149,28 @@ class CPU:
                 self.PC += 3
 
             elif IR == PUSH:
-                self.register[7] -= 1
-                self.ram[self.register[7]] = self.register[operandA]
+                self.SP -= 1
+                self.ram[self.SP] = self.register[operandA]
                 self.PC += 2
 
             elif IR == POP:
-                self.register[operandA] = self.ram[self.register[7]]
-                self.register[7] += 1
+                self.register[operandA] = self.ram[self.SP]
+                self.SP += 1
                 self.PC += 2
 
             elif IR == HLT:
                 running = False
                 self.PC += 1
+
+            elif IR == CALL:
+                self.SP -= 1
+                self.ram[self.SP] = self.PC + 2
+                self.PC = self.register[operandA]
+                
+
+            elif IR == RET:
+                self.PC = self.ram[self.SP]
+                self.SP += 1
 
             else:
                 print(f"Unknown Instruction {IR}")
